391cb1b3ab5ebd2f6056db7f5a936fd38c25c917
Vincent Rousseau <vincentrspro@gmail.com>
2025-09-26 20:53:29 +0200

    Fix SHA256 for vitte 0.1.0 tarball

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -2,7 +2,7 @@ class Vitte < Formula
   desc "Vitte programming language (Rust implementation)"
   homepage "https://github.com/vitte-lang/vitte"
   url "https://github.com/vitte-lang/vitte/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "<REMPLACE_AVEC_SHA256>"
+  sha256 "2e6a3cc46d873999b6700e5083bb0335c55baa567ecd956ff9ad088a9bef458b"
   license "MIT"
   head "https://github.com/vitte-lang/vitte.git", branch: "main"
 
