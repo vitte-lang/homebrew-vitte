9a39510734d389e48db4e4d48a3bd608101dda43
Vincent Rousseau <vincentrspro@gmail.com>
2025-09-26 20:36:11 +0200

    Add vitte 0.1.0 formula

---
+++ Formula/vitte.rb
@@ -0,0 +1,18 @@
+class Vitte < Formula
+  desc "Vitte programming language (Rust implementation)"
+  homepage "https://github.com/vitte-lang/vitte"
+  url "https://github.com/vitte-lang/vitte/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "<PASTE_SHA256_FROM_STEP_3>"
+  license "MIT"
+  head "https://github.com/vitte-lang/vitte.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/vitte", "--version"
+  end
+end
