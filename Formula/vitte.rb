b3ce91474c21028d7e08f1f9288bf04e0415133d
Vincent Rousseau <vincentrspro@gmail.com>
2025-09-26 22:49:22 +0200

    fix _depends_

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -4,10 +4,20 @@ class Vitte < Formula
   url "https://github.com/vitte-lang/vitte/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "2e6a3cc46d873999b6700e5083bb0335c55baa567ecd956ff9ad088a9bef458b"
   license "MIT"
+  head "https://github.com/vitte-lang/vitte.git", branch: "main"
 
   depends_on "rust" => :build
+  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "openssl@3"
 
   def install
-    system "cargo", "install", *std_cargo_args, "--path", "crates/vitte-cli"
+    cd "crates/vitte-cli" do
+      system "cargo", "install", *std_cargo_args
+    end
+  end
+
+  test do
+    system "#{bin}/vitte", "--version"
   end
 end
