508849c70b3f0b861ccb9a3fdb7c2b0bb6c71127
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-07 04:39:24 +0200

    add

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -4,7 +4,6 @@ class Vitte < Formula
   url "https://github.com/vitte-lang/vitte/releases/download/v0.1.0/vitte-full-v0.1.0.tar.gz"
   sha256 "3b67a51f3ab5e061e55cc3b91510a5e83c748e328343f78bbe062ceb955038f1"
   license "MIT"
-
   head "https://github.com/vitte-lang/vitte.git", branch: "main"
 
   livecheck do
@@ -13,12 +12,18 @@ class Vitte < Formula
   end
 
   def install
+    # Installe le binaire principal
     bin.install "bin/vitte"
-    pkgshare.install "share/vitte-src" => "vitte-src"
+
+    # Facultatif : installe les sources dans /usr/local/share/vitte-src
+    pkgshare.install "share/vitte-src" if Dir.exist?("share/vitte-src")
+
+    # Documentation optionnelle
+    doc.install "README.md" if File.exist?("README.md")
+    doc.install "LICENSE" if File.exist?("LICENSE")
   end
 
   test do
-    output = shell_output("#{bin}/vitte --version")
-    assert_match version.to_s, output
+    assert_match version.to_s, shell_output("#{bin}/vitte --version")
   end
 end
