f457b3fd18bfcf78666abe068421fe1f85ab75a2
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-08 00:53:22 +0200

    update

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -25,9 +25,7 @@ class Vitte < Formula
 
     manifest_dir = File.dirname(bin_pkg["manifest_path"])
     ohai "Installing from: #{manifest_dir} (package: #{bin_pkg["name"]})"
-    Dir.chdir(manifest_dir) do
-      system "cargo", "install", "--locked", "--path", ".", "--root", prefix
-    end
+    system "cargo", "install", "--locked", "--manifest-path", File.join(manifest_dir, "Cargo.toml"), "--root", prefix
   end
 
   test do
