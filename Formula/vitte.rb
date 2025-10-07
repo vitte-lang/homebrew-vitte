ac1fba5248347a983af70c7766b201c6e6c75335
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-07 04:53:44 +0200

    fix

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -1,7 +1,7 @@
 class Vitte < Formula
   desc "Vitte language CLI"
   homepage "https://github.com/vitte-lang/vitte"
-  url "https://github.com/vitte-lang/vitte/releases/download/v0.1.0/vitte-0.1.0.tar.gz"
+  url "https://github.com/vitte-lang/vitte/dist/vitte-0.1.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "Apache-2.0"
 
