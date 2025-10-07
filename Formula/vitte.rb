3603b8e699a9b6d99d7a1907b2fc8542342a5485
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-08 00:16:28 +0200

    vitte

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -1,12 +1,17 @@
 class Vitte < Formula
-  desc "Vitte language CLI"
+  desc "Vitte programming language"
   homepage "https://github.com/vitte-lang/vitte"
-  url "https://github.com/vitte-lang/vitte/dist/vitte-0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  license "Apache-2.0"
+  url "https://github.com/vitte-lang/vitte.git", branch: "main"
+  version "1.0.1"
+  license "MIT"
+
+  depends_on "rust" => :build
+  depends_on "pkg-config" => :build
+  depends_on "openssl@3"
 
   def install
-    bin.install "bin/vitte"
+    system "cargo", "build", "--release"
+    bin.install "target/release/vitte"
   end
 
   test do
