a6b42a4ad00d9218ba524f1a5eab95e831193eed
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-17 22:25:09 +0200

    vitte: ultra-simple cargo install formula

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -1,11 +1,21 @@
-def install
-  ENV["CARGO_HOME"] = (buildpath/"cargo_home").to_s
-  ENV["RUSTUP_HOME"] = (buildpath/"rustup_home").to_s
+class Vitte < Formula
+  desc "Unified Vitte language toolchain and CLI"
+  homepage "https://vitte-lang.github.io/vitte/"
+  url "https://github.com/vitte-lang/vitte.git",
+      branch: "main",
+      revision: "ba073abc56c9fa28e9d79ee84306dc1c0f07e4a9"
+  version "0.1.0"
+  license "Apache-2.0"
+  head "https://github.com/vitte-lang/vitte.git", branch: "main"
 
-  cd "crates/vitte-cli" do
-    # Pas de std_cargo_args pour éviter --path en double
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix
+    bin.install_symlink "vitte-bin" => "vitte" if (bin/"vitte-bin").exist?
   end
 
-  bin.install_symlink "vitte-bin" => "vitte" if (bin/"vitte-bin").exist?
+  test do
+    system "#{bin}/vitte", "--version"
+  end
 end
