7d6033e66eac3a0355d5a62c023c0d7fa4511ee4
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-17 16:20:43 +0200

    update

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -1,8 +1,15 @@
 class Vitte < Formula
   desc "Langage de programmation Vitte"
   homepage "https://github.com/vitte-lang/vitte"
-  head "https://github.com/vitte-lang/vitte.git", branch: "main"
+
+  # Stable depuis un tag Git (pas de tar.gz nécessaire)
+  url "https://github.com/vitte-lang/vitte.git",
+      tag:      "v0.1.0",
+      revision: "0ae6d7f41ed060a0e7321e2356353d6818033b6c"
+  version "0.1.0"
   license "MIT"
+
+  head "https://github.com/vitte-lang/vitte.git", branch: "main"
 
   depends_on "rust" => :build
 
