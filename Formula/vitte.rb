4f132787a65577def9dfb080a0864c613c1927d4
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-17 22:23:38 +0200

    vitte: cargo install without std_cargo_args to avoid duplicate --path

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -1,10 +1,10 @@
 def install
   ENV["CARGO_HOME"] = (buildpath/"cargo_home").to_s
   ENV["RUSTUP_HOME"] = (buildpath/"rustup_home").to_s
-  system "cargo", "generate-lockfile" unless File.exist?("Cargo.lock")
 
   cd "crates/vitte-cli" do
-    system "cargo", "install", *std_cargo_args
+    # Pas de std_cargo_args pour éviter --path en double
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
   bin.install_symlink "vitte-bin" => "vitte" if (bin/"vitte-bin").exist?
