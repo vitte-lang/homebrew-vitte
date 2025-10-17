7c24a5f9692178e946adea6ccd739fcfc3033356
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-17 22:26:08 +0200

    vitte: fix duplicate --path; install from crates/vitte-cli

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -11,7 +11,11 @@ class Vitte < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix
+    ENV["CARGO_HOME"] = (buildpath/"cargo_home").to_s
+    ENV["RUSTUP_HOME"] = (buildpath/"rustup_home").to_s
+    cd "crates/vitte-cli" do
+      system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    end
     bin.install_symlink "vitte-bin" => "vitte" if (bin/"vitte-bin").exist?
   end
 
