0d593c09be9c35e5f1ba26aeaa2984c16c2712e1
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-17 23:56:10 +0200

    fix: correct SHA256 for vitte 0.1.0

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -2,7 +2,7 @@ class Vitte < Formula
   desc "Unified Vitte language toolchain and CLI"
   homepage "https://vitte-lang.github.io/vitte/"
   url "https://github.com/vitte-lang/vitte/releases/download/v0.1.0/vitte-0.1.0.tar.gz"
-  sha256 "c29591c1b413398b9497210a7884508c3e2a1638ccb6c0a85232acdc138066dbf"
+  sha256 "29591c1b413398b9497210a7884508c3e2a1638ccb6c0a85232acdc138066dbf"
   license "Apache-2.0"
   head "https://github.com/vitte-lang/vitte.git", branch: "main"
 
@@ -11,12 +11,13 @@ class Vitte < Formula
   def install
     ENV["CARGO_HOME"]  = (buildpath/"cargo_home").to_s
     ENV["RUSTUP_HOME"] = (buildpath/"rustup_home").to_s
-    # la source extraite contient tout; on installe la CLI
+
     cd "crates/vitte-cli" do
-      args = ["install","--root", prefix, "--path","."]
-      args.insert(1,"--locked") if File.exist?("Cargo.lock")
+      args = ["install", "--root", prefix, "--path", "."]
+      args.insert(1, "--locked") if File.exist?("Cargo.lock")
       system "cargo", *args
     end
+
     bin.install_symlink "vitte-bin" => "vitte" if (bin/"vitte-bin").exist?
   end
 
