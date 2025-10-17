c32285ab02b5d07354123e6fab987eba1cfb8486
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-17 16:13:24 +0200

    Add formula for Vitte (builds from Git)

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -1,8 +1,7 @@
 class Vitte < Formula
   desc "Langage de programmation Vitte"
   homepage "https://github.com/vitte-lang/vitte"
-  url "https://github.com/vitte-lang/vitte/releases/download/v0.1.0/vitte-0.1.0.tar.gz"
-  sha256 "2e6a3cc46d873999b6700e5083bb0335c55baa567ecd956ff9ad088a9bef458b"
+  head "https://github.com/vitte-lang/vitte.git", branch: "main"
   license "MIT"
 
   depends_on "rust" => :build
