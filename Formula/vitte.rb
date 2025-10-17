36b8e392bd672f295ccb753287bb96c8a47586a1
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-17 15:55:09 +0200

    Update SHA256 for Vitte 0.1.0

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -2,7 +2,7 @@ class Vitte < Formula
   desc "Langage de programmation Vitte"
   homepage "https://github.com/vitte-lang/vitte"
   url "https://github.com/vitte-lang/vitte/releases/download/v0.1.0/vitte-0.1.0.tar.gz"
-  sha256 "A_REMPLACER_PAR_TA_SHA256"
+  sha256 "2e6a3cc46d873999b6700e5083bb0335c55baa567ecd956ff9ad088a9bef458b"
   license "MIT"
 
   depends_on "rust" => :build
