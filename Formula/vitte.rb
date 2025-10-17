d536bf70ef9fe13b7c9c56e391efeb342250c316
Vincent Rousseau <vincentrspro@gmail.com>
2025-10-17 21:38:42 +0200

    update

--- Formula/vitte.rb
+++ Formula/vitte.rb
@@ -1,11 +1,26 @@
-PASTE_ICI
-#
-colle
-le
-code
-ci-dessus
-à
-la
-place
-de
-PASTE_ICI
+class Vitte < Formula
+  desc "Unified Vitte language toolchain and CLI"
+  homepage "https://vitte-lang.github.io/vitte/"
+  url "https://github.com/vitte-lang/vitte.git",
+      branch: "main",
+      revision: "ba073abc56c9fa28e9d79ee84306dc1c0f07e4a9"
+  version "0.1.0"
+  license "Apache-2.0"
+  head "https://github.com/vitte-lang/vitte.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    # Génère un Cargo.lock si absent
+    system "cargo", "generate-lockfile" unless File.exist?("Cargo.lock")
+
+    # Installe le binaire depuis le sous-répertoire src
+    cd "src" do
+      system "cargo", "install", *std_cargo_args
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/vitte --version")
+  end
+end
